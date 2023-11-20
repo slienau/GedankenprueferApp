01f1a7909c4bc99efa4445586ee69ad18e39923b
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-11-20 18:49:16 +0100

    Add redux persist

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -4,13 +4,16 @@ import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 import AppNavigator from "./AppNavigator";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default () => (
   <Provider store={store}>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <AppNavigator />
-    </ApplicationProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <AppNavigator />
+      </ApplicationProvider>
+    </PersistGate>
   </Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
 import { glaubenssaetzeReducer } from "../screens/GlaubenssaetzeScreen/glaubenssaetzeSlice";
 
-export const store = configureStore({
-  reducer: {
-    glaubenssaetze: glaubenssaetzeReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage, // or your chosen storage engine
+};
+
+const rootReducer = combineReducers({
+  glaubenssaetze: glaubenssaetzeReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
