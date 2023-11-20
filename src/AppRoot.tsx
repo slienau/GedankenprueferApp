00fcbe8417da51b3d667b2dba10158ad31f455b2
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-11-20 13:14:51 +0100

    Add eva icons

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -1,13 +1,16 @@
 import * as eva from "@eva-design/eva";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+
 import AppNavigator from "./AppNavigator";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>
       <AppNavigator />
-    </Provider>
-  </ApplicationProvider>
+    </ApplicationProvider>
+  </Provider>
 );
