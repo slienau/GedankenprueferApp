71b1f3bbcd34af22e9d2d12d014b7f1889de42bb
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-11-24 10:58:58 +0100

    Edit InversionsHelpPopover

--- src/screens/GlaubenssatzPruefenScreen/InversionsHelpPopover.tsx
+++ src/screens/GlaubenssatzPruefenScreen/InversionsHelpPopover.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Button, Text } from "@ui-kitten/components";
 import { HelpIcon } from "../../ui/Icons";
 import InfoModal from "../../ui/modals/InfoModal";
@@ -30,18 +30,68 @@ export default function InversionsHelpPopover() {
       <InfoModal isVisible={visible} onClosePress={togglePopover}>
         <Text category={"p1"} style={{ marginBottom: 15 }}>
           Kehre deine Aussage um. Die Umkehrungen sind eine Möglichkeit, das
-          Gegenteil von dem zu erfahren, was du für wahr hältst. Du kannst
-          mehrere Umkehrungen finden.
+          Gegenteil von dem zu erfahren, was du für wahr hältst. Es gibt drei
+          verschiedene Arten von Umkehrungen.
         </Text>
-        <Text category={"s1"}>Beispiel einer Aussage:</Text>
-        <Text>Er hat mich verletzt.</Text>
-        <Text category={"s1"} style={{ marginTop: 15 }}>
-          Mögliche Umkehrungen:
-        </Text>
-        <Text>Ich habe mich verletzt</Text>
-        <Text>Ich habe ihn verletzt</Text>
-        <Text>Er hat mich nicht verletzt</Text>
-        <Text>Er hat mir geholfen</Text>
+
+        <View style={{ paddingVertical: 10 }}>
+          <Text category={"s1"}>Beispiel einer Aussage:</Text>
+          <Text>Er hat mich verletzt.</Text>
+        </View>
+
+        <View style={{ paddingVertical: 10 }}>
+          <Text category={"s1"}>1. Kehre den Satz ins Gegenteil um.</Text>
+          <Text>
+            Das ist möglich durch eine Verneinung des ursprünglichen Gedankens.
+          </Text>
+          <Text
+            style={{
+              textDecorationLine: "underline",
+              marginTop: 5,
+            }}
+          >
+            Mögliche Umkehrungen:
+          </Text>
+          <Text>Er hat mich nicht verletzt</Text>
+          <Text>Er hat mir geholfen</Text>
+        </View>
+
+        <View style={{ paddingVertical: 10 }}>
+          <Text category={"s1"}>2. Kehre den Satz um zur anderen Person.</Text>
+          <Text>Vertausche die handelnden Personen im Satz.</Text>
+          <Text
+            style={{
+              textDecorationLine: "underline",
+              marginTop: 5,
+            }}
+          >
+            Mögliche Umkehrung:
+          </Text>
+          <Text>
+            <Text style={{ fontStyle: "italic" }}>Ich</Text> habe{" "}
+            <Text style={{ fontStyle: "italic" }}>ihn</Text> verletzt
+          </Text>
+        </View>
+
+        <View style={{ paddingVertical: 10 }}>
+          <Text category={"s1"}>3. Kehre den Satz um zu dir selbst.</Text>
+          <Text>
+            In welcher Hinsicht hast du dich selbst so behandelt in der
+            Situation?
+          </Text>
+          <Text
+            style={{
+              textDecorationLine: "underline",
+              marginTop: 5,
+            }}
+          >
+            Mögliche Umkehrung:
+          </Text>
+          <Text>
+            <Text style={{ fontStyle: "italic" }}>Ich</Text> habe{" "}
+            <Text style={{ fontStyle: "italic" }}>mich</Text> verletzt
+          </Text>
+        </View>
       </InfoModal>
     </>
   );
