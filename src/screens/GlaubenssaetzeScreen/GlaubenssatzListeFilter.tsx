22f12c3dbf611067bb78cfc251cbe1906cad5ed7
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-11-24 18:10:42 +0100

    Finish GlaubenssatzListeFilter

--- src/screens/GlaubenssaetzeScreen/GlaubenssatzListeFilter.tsx
+++ src/screens/GlaubenssaetzeScreen/GlaubenssatzListeFilter.tsx
@@ -11,10 +11,15 @@ const GlaubenssatzListeFilter: React.FC<GlaubenssatzListeFilterProps> = ({
   filter,
   setFilter,
 }) => {
-  const [selectedIndex, setSelectedIndex] = React.useState<IndexPath[]>([
-    new IndexPath(0),
-    new IndexPath(1),
-  ]);
+  const initialIndexState = [];
+  if (filter.universelleGs) initialIndexState.push(new IndexPath(0));
+  if (filter.einschraenkendeGs) initialIndexState.push(new IndexPath(1));
+  if (filter.offenFuerZweifel) initialIndexState.push(new IndexPath(2));
+  if (filter.museumAlterGs) initialIndexState.push(new IndexPath(3));
+  if (filter.ohneStatus) initialIndexState.push(new IndexPath(4));
+
+  const [selectedIndex, setSelectedIndex] =
+    React.useState<IndexPath[]>(initialIndexState);
 
   const handleSelect = (index: IndexPath[]) => {
     setSelectedIndex(index);
@@ -36,6 +41,7 @@ const GlaubenssatzListeFilter: React.FC<GlaubenssatzListeFilterProps> = ({
       selectedIndex={selectedIndex}
       onSelect={handleSelect}
       placeholder={"Filter"}
+      value={"Filter"}
     >
       <SelectItem title="Universelle Glaubenssätze" />
       <SelectItem title="einschränkende Glaubenssätze" />
